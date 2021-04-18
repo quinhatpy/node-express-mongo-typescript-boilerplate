5b1f25da5eebed446f708e613034886086d68f52
Le Qui Nhat <quinhatpy@gmail.com>
2021-04-18 20:44:46 +0700

    feat: init project

---
+++ .eslintrc.js
@@ -0,0 +1,112 @@
+module.exports = {
+  env: {
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'prettier',
+    'standard',
+    'plugin:import/typescript',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:security/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true,
+    },
+  },
+  plugins: [
+    'prettier',
+    'import',
+    'simple-import-sort',
+    '@typescript-eslint',
+    'security',
+  ],
+  rules: {
+    'no-duplicate-imports': 'error',
+    'sort-imports': 'off',
+    'import/order': 'off',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // External packages related packages come first.
+          ['^\\w', '^@?\\w'],
+          // Internal packages.
+          [
+            '^@/.*|$',
+            '^@app/.*|$',
+            '^@app/controllers/.*|$',
+            '^@app/middleware/.*|$',
+            '^@app/models/.*|$',
+            '^@app/services/.*|$',
+            '^@configs/.*|$',
+            '^@constants/.*|$',
+            '^@databases/.*|$',
+            '^@interfaces/.*|$',
+            '^@utils/.*|$',
+            '^@routes/.*|$',
+          ],
+          [
+            // Parent imports. Put `..` last.
+            '^\\.\\.(?!/?$)',
+            '^\\.\\./?$',
+            // Other relative imports. Put same-folder imports and `.` last.
+            '^\\./(?=.*/)(?!/?$)',
+            '^\\.(?!/?$)',
+            '^\\./?$',
+          ],
+          // Style imports.
+          ['^.+\\.s?css$'],
+          // Side effect imports.
+          ['^\\u0000'],
+        ],
+      },
+    ],
+    'prettier/prettier': [
+      'warn',
+      {
+        arrowParens: 'always',
+        bracketSpacing: true,
+        semi: false,
+        trailingComma: 'all',
+        endOfLine: 'lf',
+        tabWidth: 2,
+        singleQuote: true,
+        printWidth: 80,
+        useTabs: false,
+      },
+    ],
+    'no-console': 'warn',
+    'newline-before-return': 1,
+    'no-useless-return': 1,
+    'prefer-const': 1,
+    'no-unused-vars': 0,
+    'no-throw-literal': 0,
+    'import/no-unresolved': 'error',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts'],
+      },
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json',
+      },
+    },
+  },
+}
---
+++ Dockerfile
@@ -0,0 +1,22 @@
+
+FROM node:12-alpine
+
+WORKDIR /app-api
+
+# Add package file
+COPY package.json yarn.lock ./
+
+# Install deps
+RUN yarn install --pure-lockfile
+
+# Copy source
+COPY src ./src
+COPY tsconfig.json ./tsconfig.json
+
+# Build dist
+RUN yarn build
+
+# Expose port 3000
+EXPOSE 3000
+
+CMD yarn start
